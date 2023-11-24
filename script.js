3af5f226649c2b9129499844ea61a953a083a593
Abhishek Anil <141896050+Abhishekanil007@users.noreply.github.com>
2023-11-25 05:18:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function firstWord(s) {
   // your code here
+	s = s.trimStart();
+	
+	s = s.split(' ');
+	for(let i=0; i<s.length; i++)
+		{
+			return s[0];
+		}
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+ 
